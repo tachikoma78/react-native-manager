f12517d47655487c03b04eafffc3e07b2d8411db
Pierre <pierrevanderpol@gmail.com>
2018-02-05 08:57:21 +0100

    initial config

--- manager/App.js
+++ manager/App.js
@@ -1,57 +1,34 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+import firebase from 'firebase';
 import {
-  Platform,
-  StyleSheet,
   Text,
   View
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
 export default class App extends Component<{}> {
+  componentWillMount(){
+    const config = {
+      apiKey: "AIzaSyCvbV9mVswI1QlRr_eVsakJ2IEo3O_rZl0",
+      authDomain: "manager-404ba.firebaseapp.com",
+      databaseURL: "https://manager-404ba.firebaseio.com",
+      projectId: "manager-404ba",
+      storageBucket: "",
+      messagingSenderId: "928055502172"
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View>
+            <Text>
+              Hello!
+            </Text>
+        </View>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
